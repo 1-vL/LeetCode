f9b27ca56cb4f9392fc8d12c1bca686c1bfbb696
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-23 00:28:56 +0900

    Time: 1 ms (38.52%), Space: 41.4 MB (36.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i=0;i<n;i++) {
-            nums1[m+i] = nums2[i];
+        for (int j = 0, i = m; j < n; j++) {
+        // 거의 동일하지만 더 깔끔하고 좁은 범위의 for 문
+            nums1[i] = nums2[j];
+            i++;
         }
         Arrays.sort(nums1);
     }
