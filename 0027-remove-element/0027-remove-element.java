9b9c0003df4c24671eb7a639fa2f4351e50f9535
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-23 01:38:02 +0900

    Time: 0 ms (100.00%), Space: 41 MB (69.22%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,8 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int[] tempnums = Arrays.stream(nums).filter(i -> i != val).toArray();
-        int k = tempnums.length;
-        System.arraycopy(tempnums, 0, nums, 0, k);
+        int val_index = -1;
+        int k = 0;
+        boolean start = false;
+        for (int i=0; i<nums.length; i++) {
+            if (val == nums[i]) {
+                if (!start) {
+                    val_index = i;
+                    start = true;                    
+                }
+            } else {
+                k++;
+            }
+            if (start && val != nums[i]) {
+                nums[val_index++] = nums[i];
+            }
+        }
         return k;
     }
 }
