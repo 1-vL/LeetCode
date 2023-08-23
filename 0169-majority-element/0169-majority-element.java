0a985c3f6882ab7f977e23bf0dae2292215b0b66
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-23 21:55:43 +0900

    Time: 4 ms (43.50%), Space: 49 MB (12.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority = nums.length/2+1;
+        int len = nums.length;
         int pos = 0;
         Arrays.sort(nums);
         
-        for (int i=1;i<nums.length;i++) {
+        for (int i=1;i<len;i++) {
             if (nums[i]!=nums[i-1]) {
                 // 서로 다른 숫자 경계지점
-                if ((i-pos)>=majority) {
+                if ((i-pos)>=len/2+1) {
                     return nums[i-1];
                 }
                 pos = i-1;
             }
         }
-        return nums[nums.length-1];
+        return nums[len-1];
     }
 }
