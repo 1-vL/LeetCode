a5cdda469dcfa8214b70d4ba794af65dcd711f49
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-23 23:31:55 +0900

    Time: 0 ms (100.00%), Space: 55.7 MB (66.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,12 +1,17 @@
 
 class Solution {
     public void rotate(int[] nums, int k) {
-        // 요소 하나짜리 배열이거나 이동이 0이면 이동의 의미 없음
+        // 요소 하나짜리 배열이면 이동의 의미 없음
         if (nums.length == 1 || k == 0) {
             return;
         }
         // 이동 수가 배열 길이보다 크면 나머지만큼만 이동
         k = k % nums.length;
+        
+        if (k == 0) {
+        // 한바퀴 돌아서 이동이 0인지 체크
+            return;
+        }
         
         int[] temp = new int[k];
         System.arraycopy(nums,nums.length-k,temp,0,temp.length);
