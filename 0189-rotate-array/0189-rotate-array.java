6458f3de5efafdf9b875807648d3e4c6e0d55512
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-24 16:52:40 +0900

    Time: 0 ms (100.00%), Space: 56.3 MB (21.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,25 +1,26 @@
-/* Time complexity is O(nlog(n)) */
 class Solution {
-    public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        if (n <= 1) {
-            return;
-        }
-        
-        k = k % n;
-        
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+
+    public static void reverse(int nums[], int start, int end){
+    // While start index is less than end index
+    while(start < end){
+        // Swap elements at start and end indices
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        // Increment start index and decrement end index
+        start++;
+        end--;
     }
-    
-    public static void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
+}
+
+    public void rotate(int[] nums, int k) {
+        // Ensure k is within array bounds
+        k %= nums.length;
+        // Reverse entire array
+        reverse(nums, 0, nums.length - 1);
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse remaining elements
+        reverse(nums, k, nums.length - 1);
     }
 }
