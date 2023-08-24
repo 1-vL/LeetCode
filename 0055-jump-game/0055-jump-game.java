314c16c24f37d7efe5532db390eba3ccb28d2566
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-24 18:22:19 +0900

    Time: 2 ms (80.25%), Space: 44.3 MB (54.48%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length==1) {
+            return true;
+        }
+        
+        int possible = nums[0];
+        for (int i=0;i<nums.length-1;i++) {
+            if (possible >= i) {
+                possible = Math.max(nums[i]+i, possible);
+            }
+            if (possible >= nums.length-1) {
+                return true;
+            }
+        }
+        // System.out.println(possible);
+        return false;
+    }
+}
