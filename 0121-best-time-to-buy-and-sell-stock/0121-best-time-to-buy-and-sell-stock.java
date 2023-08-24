bef8bcbcf503bddd1896d59864c91964afa94669
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-24 17:40:24 +0900

    Time: 1 ms (99.91%), Space: 61.4 MB (16.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -5,6 +5,16 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max_pos = 0;
         int min_pos = 0;
+        
+        int real_min = Integer.MAX_VALUE;
+        int real_max = -1;
+        // 배열 내부의 최대값과 최소값 찾기
+        for (int i=0;i<prices.length;i++) {
+            real_min = Math.min(real_min, prices[i]);
+            real_max = Math.max(real_max, prices[i]);
+        }
+        int real_max_profit = real_max-real_min;
+        
         for (int i=0;i<prices.length;i++) {
             int now = prices[i];
             // 최고가 갱신
@@ -14,6 +24,9 @@ class Solution {
             }
             // 차익 계산
             max_profit = Math.max(max_profit, max-min);
+            if (real_max_profit == max_profit) {
+                return real_max_profit;
+            }
             // 새로운 최저가 -> 이전 최고가 사용할 의미 없어지므로 초기화
             if (now < min) {
                 min_pos = i;
