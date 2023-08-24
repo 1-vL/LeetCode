d4217c26cd99436da07b9bbeb8771fca82af80e4
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-24 16:52:06 +0900

    Time: 2 ms (19.45%), Space: 56.5 MB (6.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,25 @@
-
+/* Time complexity is O(nlog(n)) */
 class Solution {
-    public void rotate(int[] nums, int k) {
-        // 요소 하나짜리 배열이면 이동의 의미 없음
-        if (nums.length == 1 || k == 0) {
-            return;
-        }
-        // 이동 수가 배열 길이보다 크면 나머지만큼만 이동
-        k = k % nums.length;
-        
-        if (k == 0) {
-        // 한바퀴 돌아서 이동이 0인지 체크
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        if (n <= 1) {
             return;
         }
         
-        int[] temp = new int[k];
-        System.arraycopy(nums,nums.length-k,temp,0,temp.length);
-        System.arraycopy(nums,0,nums,temp.length,nums.length-k);
-        System.arraycopy(temp,0,nums,0,temp.length);
+        k = k % n;
+        
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
