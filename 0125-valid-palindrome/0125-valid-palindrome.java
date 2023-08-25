eaa0213a1633c12cb23018846c995749a29ecf1d
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-25 22:56:16 +0900

    Time: 3 ms (87.05%), Space: 43.5 MB (57.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -19,7 +19,7 @@ class Solution {
     }
     
     public String trim(String s) {
-        s=s.toLowerCase();
+        s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
         for (int i=0;i<s.length();i++) {            
             char c = s.charAt(i);
