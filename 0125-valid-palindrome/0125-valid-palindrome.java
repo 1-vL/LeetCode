9367a4765b8cba7a32d59a94e5f4240170825b7e
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-25 23:49:53 +0900

    Time: 2 ms (99.64%), Space: 42.1 MB (73.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,56 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        // 문자만 남기기
-        s = trim(s);
+class Solution{
+public  boolean isPalindrome(String s) {
+        // int i = 0, j = s.length() - 1;
+        // 0 9  A Z  a z
         
-        // 빈 문자열인 경우 true
-        if (s.length() == 0) {
-            return true;
-        }
-        
-        // 투포인터로 비교후 다른 경우 즉시 false 리턴
-        for (int i=0;i<s.length()/2;i++) {
-            if (s.charAt(i) != s.charAt(s.length()-1-i)) {
-               return false; 
+        char[] c = s.toCharArray();
+        int i = 0, j = c.length - 1;
+        // if(c.length==2){
+        //     return c[i]==c[j]?true:false;
+        // }
+        while (i <=j) {
+            if (upper(c[i])) {
+                c[i] = tolowercase(c[i]);
             }
+            if (upper(c[j])) {
+                c[j] = tolowercase(c[j]);
+            }
+            i++;
+            j--;
         }
-        // 투포인터 통과시 회문
+        // 2 -1
+        // System.out.println(Arrays.toString(c));
+        i = 0;
+        j = c.length - 1;
+        while (i < j) {
+            while (i<c.length&&!isalphanu(c[i])) {
+                i++;
+            }
+            while (i<c.length&&!isalphanu(c[j])) {
+                j--;
+            }
+            if(j<i){
+                return true;
+            }
+            if (c[i] != c[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
         return true;
     }
-    
-    public String trim(String s) {
-        s=s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        for (int i=0;i<s.length();i++) {            
-            char c = s.charAt(i);
-            // 숫자 또는 알파벳 소문자만
-            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+
+    public  char tolowercase(char c) {
+        return (char) (c + 32);
+    }
+
+    public  boolean isalphanu(char c) {
+        boolean b = (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ? true : false;
+        return b;
+    }
+
+    public  boolean upper(char c) {
+        return (c >= 'A' && c <= 'Z') ? true : false;
     }
 }
