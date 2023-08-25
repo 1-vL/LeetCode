9582d9d37865437464de02eb21caf3fb7aa3bd99
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-25 23:01:47 +0900

    Time: 3 ms (87.05%), Space: 43.6 MB (57.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -20,7 +20,7 @@ class Solution {
         
         // 투포인터로 비교후 다른 경우 즉시 false 리턴
         for (int i=0;i<len/2;i++) {
-            if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+            if (s.charAt(i) != s.charAt(len-1-i)) {
                return false; 
             }
         }
