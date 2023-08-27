8e1c5a536f1254b0ffd4936679424aab0664a818
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-28 02:37:56 +0900

    Time: 2 ms (58.23%), Space: 45.5 MB (62.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,12 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int min_idx = 0;
-        int max_idx = numbers.length-1;
-        
-        for (int i=0;min_idx<max_idx;i++) {
-            int min = numbers[min_idx];
-            int max = numbers[max_idx];
-            if (min + max == target && (min_idx != max_idx)) {
-                return new int[] {min_idx+1, max_idx+1};
-            } else if (min + max < target) {
-                min_idx++;
-            } else if (min + max > target) {
-                max_idx--;
-            }
+    public int[] twoSum(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (nums[l] + nums[r] != target) {
+            if (nums[l] + nums[r] < target) l++;
+            else r--;
         }
-        return new int[] {-1,-1};
+
+        return new int[] {l+1, r+1};
     }
 }
