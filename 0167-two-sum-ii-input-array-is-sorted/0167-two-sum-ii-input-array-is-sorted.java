0f47fece799f64c3c9cc45572f711c850a929e23
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-28 01:56:48 +0900

    Time: 2 ms (58.23%), Space: 46.1 MB (5.63%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,8 +3,8 @@ class Solution {
         int min_idx = 0;
         int max_idx = numbers.length-1;
         
-        // 반드시 정답이 1개 존재한다고 조건을 제시해 줬기 때문에 그냥 true로 설정할 수도 있겠지만 일단 배열 범위를 벗어나면 종료되도록 설정
-        while (0 <= min_idx && min_idx < numbers.length && 0 <= max_idx && max_idx < numbers.length) {
+        // 반드시 정답이 1개 존재한다고 조건을 제시해 줬기 때문에 성능을 위해 그냥 true로 설정
+        while (true) {
             if (numbers[min_idx] + numbers[max_idx] == target) {
                 return new int[] {min_idx+1,max_idx+1};
             } else if (numbers[min_idx] + numbers[max_idx] < target) {
@@ -13,7 +13,5 @@ class Solution {
                 max_idx--;
             }
         }
-        // 위에서 정답을 리턴할 예정이므로 정답이 없는 테스트 케이스가 존재한다면 이 값을 리턴했을 것이다.
-        return new int[] {-1,-1};
     }
 }
