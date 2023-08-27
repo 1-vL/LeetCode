f432599a9fe6a607678a52bf458be67c47675ec8
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-28 02:25:26 +0900

    Time: 2 ms (58.23%), Space: 45.3 MB (84.91%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,7 +3,7 @@ class Solution {
         int min_idx = 0;
         int max_idx = numbers.length-1;
         
-        for (int i=0;min_idx<max_idx;i++) {
+        while(min_idx<max_idx) {
             int min = numbers[min_idx];
             int max = numbers[max_idx];
             if (min + max == target && (min_idx != max_idx)) {
