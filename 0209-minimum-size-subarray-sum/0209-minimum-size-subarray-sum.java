ef571b6b01a10895ff92fe04838033f7629c3205
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-29 00:30:50 +0900

    Time: 1 ms (99.96%), Space: 55.6 MB (65.67%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,24 +1,24 @@
-public class Solution {
-    public int minSubArrayLen(int s, int[] nums) {
-        int i = 1, j = nums.length, min = 0;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (windowExist(mid, nums, s)) {
-                j = mid - 1;
-                min = mid;
-            } else i = mid + 1;
-        }
-        return min;
-    }
-
-
-    private boolean windowExist(int size, int[] nums, int s) {
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i >= size) sum -= nums[i - size];
-            sum += nums[i];
-            if (sum >= s) return true;
+        int cnt = Integer.MAX_VALUE;
+        
+        // 배열 길이가 1인 경우 및 첫번째 요소가 조건을 만족시키는 경우를 빠르게 체크하기 위해서 단 하나 있는 요소가 타겟보다 크다면 즉시 1 리턴
+        if (target <= nums[0]) {
+            return 1;
         }
-        return false;
+            
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];       
+            while (target <= sum) {
+                cnt = Math.min(cnt, -start + end + 1);
+                sum -= nums[start++];
+            } 
+        }
+        if (cnt == Integer.MAX_VALUE) {
+            cnt = 0;
+        }
+        return cnt;
     }
 }
