3a545a08d4e622919c037dde5dd4c1157435af92
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-30 22:46:52 +0900

    Time: 3 ms (91.64%), Space: 41.8 MB (94.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,9 @@
-class Solution {
+public class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] alpha = new int[26];
-        
-        // s의 알파벳 집계
-        for (int i=0;i<s.length();i++) {
-            alpha[s.charAt(i) - 'a']++;
-        }
-
-        // t 만드는데 사용한 경우 제거
-        for (int i=0;i<t.length();i++) {
-            if (--alpha[t.charAt(i) - 'a'] < 0) return false;
-            // 사용한 후 음수값이 되면 즉시 false 리턴
-        }
-        // 사용하고 남은 알파벳이 있어도 false
-        for (int i=0;i<26;i++) {
-            if (alpha[i]!=0) return false;
-        }
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
         return true;
-        
     }
 }
