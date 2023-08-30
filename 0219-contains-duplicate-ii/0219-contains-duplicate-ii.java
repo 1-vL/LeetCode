4c897df404f336757b4e35605ae29a06e0262a97
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-30 21:27:52 +0900

    Time: 13 ms (99.41%), Space: 56.8 MB (48.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,21 +1,10 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // 최대 K 만큼 떨어진 범위 안에 존재하는 서로 다른
-        // 두 요소가 중복인 경우가 존재하는지 체크하고 리턴
-
-        // 슬라이딩 윈도우 + HashSet으로 해결 가능할듯
-        if (k==0) return false;
-        HashSet<Integer> set = new HashSet<Integer>();        
-        for (int i=0;i<Math.min(k,nums.length);i++) {
-            if (set.contains(nums[i])) return true;
-            set.add(nums[i]);
-        }
-
-        for (int i=0;i<nums.length-k;i++) {
-            if (set.contains(nums[i+k])) return true;
-            set.remove(nums[i]);
-            set.add(nums[i+k]);
-        }
-        return false;
+            Set<Integer> set = new HashSet<Integer>();
+            for(int i = 0; i < nums.length; i++){
+                if(i > k) set.remove(nums[i-k-1]); //remove element if its distance to nums[i] is not lesser than k
+                if(!set.add(nums[i])) return true; //because all still existed elements is closer than k distance to the num[i], therefore if the add() return false, it means there's a same value element already existed within the distance k, therefore return true.
+            }
+            return false;
     }
 }
