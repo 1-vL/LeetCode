7db275ab8c26017066ed89bf8f34d52dc79aa88a
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-30 22:33:13 +0900

    Time: 2 ms (95.82%), Space: 43.2 MB (98.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -11,7 +11,8 @@ class Solution {
 
         // ransomNote 만드는데 사용한 경우 제거
         for (int i=0;i<rn.length;i++) {
-            if (--alpha[rn[i] - 'a'] < 0) return false; // 사용한 후 음수값이 되면 즉시 false 리턴
+            if (--alpha[rn[i] - 'a'] < 0) return false;
+            // 사용한 후 음수값이 되면 즉시 false 리턴
         }
         return true;
     }
