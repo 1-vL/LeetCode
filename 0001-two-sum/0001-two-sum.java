ba7ee5779a370a18bdba4ddc6284531d1c2bc392
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-30 19:46:01 +0900

    Time: 4 ms (66.24%), Space: 43.5 MB (89.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,10 +5,10 @@ class Solution {
             map.put(nums[i],i);
         }
         for (int i=0;i<nums.length;i++) {
-            int num2 = target-nums[i]; // 현재 숫자 nums[i]와 합하여 target이 되는 숫자
-            int num2_idx = map.getOrDefault(num2, -1); // num2의 인덱스값 (존재한다면 해당 인덱스, 존재하지 않는다면 -1 리턴)
+            // int num2 = target-nums[i]; // 현재 숫자 nums[i]와 합하여 target이 되는 숫자
+            int num2_idx = map.getOrDefault(target-nums[i], -1); // num2의 인덱스값 (존재한다면 해당 인덱스, 존재하지 않는다면 -1 리턴)
             if (num2_idx == -1) continue; // num2 없으면 continue;            
-            if (nums[i]!=num2 || i!=num2_idx) { // 요소를 중복사용하지 않았다면 정답
+            if (nums[i]!=target-nums[i] || i!=num2_idx) { // 요소를 중복사용하지 않았다면 정답
                 return new int[] {i, num2_idx}; // 순서는 상관 없으므로 정답이 나오는 순간 즉시 리턴
             }
         }
