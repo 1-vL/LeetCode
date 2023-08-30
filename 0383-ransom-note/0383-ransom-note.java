d3e2281ee5f12a192780f923da6489eb8871192e
Jun <92934376+1-vL@users.noreply.github.com>
2023-08-30 22:32:31 +0900

    Time: 2 ms (95.82%), Space: 43.5 MB (87.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        char[] rn = ransomNote.toCharArray();
+        int[] alpha = new int[26];
         char[] mg = magazine.toCharArray();
+        char[] rn = ransomNote.toCharArray();
+        
+        // magazine의 알파벳 집계
         for (int i=0;i<mg.length;i++) {
-            map.put(mg[i], map.getOrDefault(mg[i],-1)+1);
+            alpha[mg[i] - 'a']++;
         }
+
+        // ransomNote 만드는데 사용한 경우 제거
         for (int i=0;i<rn.length;i++) {
-            if (!map.containsKey(rn[i])) return false;
-            int cnt = map.put(rn[i], map.getOrDefault(rn[i],0)-1);
-            if (cnt < 0) return false;
+            if (--alpha[rn[i] - 'a'] < 0) return false; // 사용한 후 음수값이 되면 즉시 false 리턴
         }
         return true;
     }
