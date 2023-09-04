86ff08b15dea8f52cb93488b3d87750eaae4f9ca
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-04 21:39:51 +0900

    Time: 0 ms (100%), Space: 40.8 MB (94.16%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,16 +4,16 @@ class Solution {
         // 1개짜리 배열이면 즉시 리턴
         if (nums.length == 1) { return 0; }
 
-        // 이진 탐색 시 start, end, mid 위치의 세 값 중 가장 큰 값을 가진 쪽으로 이동시키기
+        // 이진 탐색 시 mid 위치의 값을 기준으로 Peak 가능성 체크 후 가능한 쪽 탐색
         while (start < end) {
-            mid = (start + end) /2; // 중간 위치
-            if (nums[mid]<nums[mid+1]) { // 중간이 자기 옆 칸보다 작다면 -> 피크 가능성 없음
+            mid = (start + end) / 2; // 중간 위치
+            if (nums[mid]<nums[mid+1]) { // 중간이 자기 옆 칸보다 작다면 -> 중간은 피크 가능성 없음
                 start = mid+1; // 범위 시작을 중간 옆칸으로
             } else { // 중간이 자기 옆칸 이상이라면
                 end = mid; // 끝을 중간 자리로
             }
         } 
         // 탈출 시점에서 start >= end
-        return start;
+        return end;
     }
 }
