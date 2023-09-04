c9322c11c12a2afc499b99a991a82fc9c9c66764
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-04 20:41:19 +0900

    Time: 0 ms (100%), Space: 41 MB (76.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,12 +2,12 @@ class Solution {
     public int findPeakElement(int[] nums) {
         // 1개짜리 배열이면 즉시 0리턴
         if (nums.length == 1) { return 0; }
+
         // 최대값 찾기
         int max = Integer.MIN_VALUE;
         int pos = -1;
-        int half = nums.length/2;
         // 배열 요소 수의 절반만큼의 횟수 반복
-        for (int i=0;i<half+1;i++) {            
+        for (int i=0;i<nums.length/2+1;i++) {            
             // 1번 반복 시 2개 요소 확인
             if (2*i<nums.length && max<nums[2*i]) {
                 max=nums[2*i];
