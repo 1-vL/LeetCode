f58f18cfe3bdf6518656a8adc21ceda8544319bf
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-04 21:28:51 +0900

    Time: 0 ms (100%), Space: 41.2 MB (52.07%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,19 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // 1개짜리 배열이면 즉시 0리턴
+        int start = 0, end = nums.length-1, mid = 0;
+        // 1개짜리 배열이면 즉시 리턴
         if (nums.length == 1) { return 0; }
 
-        // 최대값 찾기
-        int max = Integer.MIN_VALUE;
-        int pos = -1;
-        // 배열 요소 수의 절반만큼의 횟수 반복
-        for (int i=0;i<nums.length/2+1;i++) {            
-            // 1번 반복 시 2개 요소 확인
-            if (2*i<nums.length && max<nums[2*i]) {
-                max=nums[2*i];
-                pos=2*i;
-                }
-            if (2*i+1<nums.length && max<nums[2*i+1]) {
-                max=nums[2*i+1];
-                pos=2*i+1;
-                }
-        }
-        return pos;
+        // 이진 탐색 시 start, end, mid 위치의 세 값 중 가장 큰 값을 가진 쪽으로 이동시키기
+        while (start < end) {
+            mid = (start + end) /2; // 중간 위치
+            if (nums[mid]<nums[mid+1]) { // 중간이 자기 옆 칸보다 작다면 -> 피크 가능성 없음
+                start = mid+1; // 범위 시작을 중간 옆칸으로
+            } else { // 중간이 자기 옆칸 이상이라면
+                end = mid; // 끝을 중간 자리로
+            }
+        } 
+        // 탈출 시점에서 start >= end
+        return start;
     }
 }
