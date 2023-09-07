56e25740358a9c19ef46c3ce5c90fac6694dc828
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 22:39:13 +0900

    Time: 4 ms (12.78%), Space: 44.7 MB (38.08%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> answer = new ArrayList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
+        Double sum = 0.0;
+        int cnt = 1; // root
+        int level_cnt = 1; // root
+
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode now = q.poll();
+            level_cnt--;
+            
+            sum += now.val;
+            if (now.left!=null) {
+                q.offer(now.left);
+            }
+
+            if (now.right!=null) {
+                q.offer(now.right);
+            }
+
+            if (level_cnt==0) {
+                answer.add(sum/cnt);
+                sum = 0.0;
+                level_cnt = q.size();
+                cnt = q.size();
+            }
+        }
+
+        return answer;
+    }
+}
