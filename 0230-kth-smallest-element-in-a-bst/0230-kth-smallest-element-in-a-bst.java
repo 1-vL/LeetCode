a686faf4b9057f57d86c0bf463a8918d17910f4a
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 20:27:17 +0900

    Time: 0 ms (100%), Space: 44.8 MB (7.35%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,19 +1,18 @@
 class Solution {
-    public static int ans = 0;
+    int cnt=0;
+    int kcnt;
+    int answer;
     public int kthSmallest(TreeNode root, int k) {
-        helper(root, k);
-        return ans;
+        kcnt = k;
+        inOrder(root);
+        return answer;
     }
 
-    public int helper(TreeNode root, int k) {
-        if (root == null) {
-            return 0;
-        }
-        int leftCount = helper(root.left, k); // k번째 요소 탐색
-        int rightCount = helper(root.right, k - leftCount - 1); // 왼쪽 하위노드에서 체크한 수 만큼 제외한 k - leftCount - 1번째 요소 탐색
-        if (k == leftCount + 1) { // 정답 찾음(1인덱스)
-            ans = root.val;
-        }
-        return leftCount + rightCount + 1; // 좌우 노드의 체크한 요소들 갯수의 합
+    public void inOrder(TreeNode node) {
+        if (node == null || cnt > kcnt) { return; }
+        inOrder(node.left);
+        cnt++;
+        if (cnt==kcnt) { answer=node.val; return; }
+        inOrder(node.right);
     }
 }
