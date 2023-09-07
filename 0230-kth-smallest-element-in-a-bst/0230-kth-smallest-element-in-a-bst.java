0dc9b7a17804a047defe9a0f8923ffd47f6dfd3a
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 20:26:52 +0900

    Time: 0 ms (100%), Space: 43.5 MB (98.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -9,11 +9,11 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        int leftCount = helper(root.left, k);
-        int rightCount = helper(root.right, k - leftCount - 1);
-        if (k == leftCount + 1) {
+        int leftCount = helper(root.left, k); // k번째 요소 탐색
+        int rightCount = helper(root.right, k - leftCount - 1); // 왼쪽 하위노드에서 체크한 수 만큼 제외한 k - leftCount - 1번째 요소 탐색
+        if (k == leftCount + 1) { // 정답 찾음(1인덱스)
             ans = root.val;
         }
-        return leftCount + rightCount + 1;
+        return leftCount + rightCount + 1; // 좌우 노드의 체크한 요소들 갯수의 합
     }
 }
