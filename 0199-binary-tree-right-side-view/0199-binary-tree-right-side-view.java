a3a0da05f5da91066d10e87ef8844986433f372e
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 21:30:59 +0900

    Time: 1 ms (65.57%), Space: 41.1 MB (64.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -3,13 +3,13 @@ class Solution {
         List<Integer> answer = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
         int level_pushed = 1; // root 요소 1개
-        int level_polled = 0;
+
         if (root == null) { return answer; }
 
         q.offer(root);
         while (!q.isEmpty()) {
             TreeNode now = q.poll();
-            level_polled++;
+            level_pushed--;
 
             if (now.left!=null) {
                 q.offer(now.left);
@@ -17,11 +17,10 @@ class Solution {
             if (now.right!=null) {
                 q.offer(now.right);
             }
-            if (level_pushed==level_polled) { // 한 높이의 마지막 요소
+            if (level_pushed==0) { // 한 높이의 마지막 요소
                 answer.add(now.val);
                 // 높이 초기화
                 level_pushed = q.size();
-                level_polled = 0;
             }
         }
         return answer;
