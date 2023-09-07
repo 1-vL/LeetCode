d7151b2b49d0e8471e65f385245a4a5e9731b645
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 18:59:43 +0900

    Time: 0 ms (100%), Space: 43.1 MB (69.24%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -1,25 +1,20 @@
 class Solution {
-    static int min;
-    static int before_val;
+    int min=Integer.MAX_VALUE;
+    int before_val=-1;
     public int getMinimumDifference(TreeNode root) {
-        // static 변수 초기화
-        before_val=-1;
-        min  = Integer.MAX_VALUE;
-        Inorder(root); // 루트노드 중위순회
+        inOrder(root); // 루트노드 중위순회
         return min;
     }
-        
     
-    void Inorder(TreeNode node) {
+    void  inOrder(TreeNode node) {
         if(node == null) { return; } // 리프노드
-        Inorder(node.left); // 왼쪽 재귀
+        inOrder(node.left); // 왼쪽 재귀
         // 이전 값이 존재하면
         if (before_val>=0) {
             // 오름차순 순회이므로 현재값에서 이전값을 뺀 값(차이)가 min보다 작은지 체크
             min = Math.min(min, node.val-before_val);
         }
-        System.out.println(node.val + " " + before_val);
         before_val = node.val;
-        Inorder(node.right); // 오른쪽 재귀
+        inOrder(node.right); // 오른쪽 재귀
     }
 }
