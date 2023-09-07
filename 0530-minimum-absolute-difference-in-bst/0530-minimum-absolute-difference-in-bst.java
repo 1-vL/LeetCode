54a2c3cfa239dbe3305e53c79c13c71aebfcc823
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-07 16:19:36 +0900

    Time: 6 ms (7.94%), Space: 43.1 MB (54.83%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -30,10 +30,10 @@ class Solution {
                 q.offer(now.right);
             }
         }
-        Integer[] array = list.toArray(new Integer[0]);
-        Arrays.sort(array);
-        for (int i=0; i<array.length-1; i++) {
-                min = Math.min(min, array[i+1]-array[i]);
+        // Integer[] array = list.toArray(new Integer[0]);
+        Collections.sort(list);
+        for (int i=0; i<list.size()-1; i++) {
+                min = Math.min(min, list.get(i+1)-list.get(i));
         }
         return min;
     }
