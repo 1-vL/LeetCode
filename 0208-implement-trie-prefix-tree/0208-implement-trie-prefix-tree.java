9f670a24ef81c0ddc7b9212a0e47e6afe1eef23e
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-11 17:38:20 +0900

    Time: 38 ms (57.33%), Space: 54.7 MB (55.39%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,81 +1,50 @@
-class Trie {
-
-    Node head;
-    Node dummy = new Node(); // Dummy Head용 빈 노드
-
-    class Node { // Node 클래스
-        HashMap<Character, Node> children;
-        boolean end = false;
-
-        Node() {
-            this.children = new HashMap<Character, Node>();
-            this.end = false;
-        }
-    }
-
-    public Trie() {
-        head = dummy;
-    }
-    
-    public void insert(String word) {
-        HashMap<Character, Node> next = head.children; // 더미 헤드의 칠드런(다음 노드들)
-
-        for (int i=0;i<word.length();i++) { // 문자열을 순회하며 알파벳 체크
-            char now = word.charAt(i); // 지금 체크하는 알파벳
-            if (next.containsKey(now)) { // 다음 노드들중에 이 알파벳이 포함되어 있는지
-                // 이미 존재하는 노드
-                if (i==word.length()-1) { // 현재 문자열의 마지막 문자라면
-                    next.get(now).end = true; // end를 true로 변경
-                }
-                next = next.get(now).children; // 다음 노드들 현재 노드의 children으로 변경
-            } else {
-                // 아직 이 문자의 노드가 없다면
-                Node input = new Node(); // 추가할 노드
-                if (i==word.length()-1) { input.end = true; } // 마지막 문자라면 end = true;
-                next.put(now, input); // 추가
-                next = next.get(now).children; // 다음 노드들 현재 추가한 노드의 children으로 변경
-            }
-        }
-    }
-    
-    public boolean search(String word) {
-        HashMap<Character, Node> next = head.children; // 더미 헤드의 칠드런(다음 노드들)
-        for (int i=0;i<word.length();i++) {
-            char now = word.charAt(i);
-            if (next.containsKey(now)) { // 키가 있다면
-                if (i == word.length()-1) { // 마지막 문자열이라면
-                    if (next.get(now).end) { // end가 true라면(정확히 이 문자열이 입력된 적이 있다면)
-                        return true;
-                    } else {                         
-                        return false;
-                    }
-                }
-                next = next.get(now).children; // 다음 노드들 현재 추가한 노드의 children으로 변경
-            } else {                
-                return false; // 없다면 바로 false 리턴하여 탈출
-            }
-        }
-        return true; // 끝까지 체크 후 false 아니라면 true
-    }
-    
-    public boolean startsWith(String prefix) {
-        HashMap<Character, Node> next = head.children; // 더미 헤드의 칠드런(다음 노드들)
-        for (int i=0;i<prefix.length();i++) {
-            char now = prefix.charAt(i);
-            if (next.containsKey(now)) { // 키가 있다면
-                next = next.get(now).children; // 다음 노드 탐색
-            } else {
-                return false; // 없다면 바로 false 리턴하여 탈출
-            }
-        }
-        return true; // 끝까지 체크 후 false 아니라면 true
+class TrieNode {
+    public char val;
+    public boolean isWord; 
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {}
+    TrieNode(char c){
+        TrieNode node = new TrieNode();
+        node.val = c;
     }
 }
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
+public class Trie {
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+        root.val = ' ';
+    }
+
+    public void insert(String word) {
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null){
+                ws.children[c - 'a'] = new TrieNode(c);
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode ws = root; 
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return ws.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode ws = root; 
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return true;
+    }
+}
