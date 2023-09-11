f105c532ce7ca69d7cd8836a21274123acfb2f80
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-11 17:41:21 +0900

    Time: 34 ms (83.07%), Space: 54.3 MB (90.77%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,50 +1,46 @@
 class TrieNode {
-    public char val;
-    public boolean isWord; 
-    public TrieNode[] children = new TrieNode[26];
-    public TrieNode() {}
-    TrieNode(char c){
-        TrieNode node = new TrieNode();
-        node.val = c;
-    }
+boolean isEnd;
+TrieNode[] children;
+
+public TrieNode() {
+    isEnd = true;
+    children = new TrieNode[26];
+}
 }
 
 public class Trie {
-    private TrieNode root;
-    public Trie() {
-        root = new TrieNode();
-        root.val = ' ';
-    }
+private TrieNode root;
 
-    public void insert(String word) {
-        TrieNode ws = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null){
-                ws.children[c - 'a'] = new TrieNode(c);
-            }
-            ws = ws.children[c - 'a'];
-        }
-        ws.isWord = true;
-    }
+public Trie() {
+    root = new TrieNode();
+}
 
-    public boolean search(String word) {
-        TrieNode ws = root; 
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
-        }
-        return ws.isWord;
+public void insert(String word) {
+	TrieNode current = root;
+	for(int i=0, L=word.length(); i<L; i++) {
+    	int id = word.charAt(i) - 'a';
+    	if(current.children[id]==null) {
+    		current.children[id] = new TrieNode();
+    		current.children[id].isEnd = false;
+    	}
+    	current = current.children[id];
     }
+    current.isEnd = true;
+}
 
-    public boolean startsWith(String prefix) {
-        TrieNode ws = root; 
-        for(int i = 0; i < prefix.length(); i++){
-            char c = prefix.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
-        }
-        return true;
+public boolean search(String word) {
+    return search(word, 1);
+}
+public boolean startsWith(String prefix) {
+    return search(prefix, 2);
+}
+private boolean search(String str, int type) {
+    TrieNode current = root;
+    int i=-1, L=str.length();
+    while(++i<L) {
+        int id = str.charAt(i) - 'a';
+        if((current=current.children[id]) == null) return false;
     }
+    return type==1 ? current.isEnd : true;
+}
 }
