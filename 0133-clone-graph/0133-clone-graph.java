7555d821ef7cc277a34dd2657d16f383f3c695d4
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-14 16:43:18 +0900

    Time: 26 ms (64.46%), Space: 42.1 MB (34.6%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,46 +1,20 @@
 class Solution {
+    public HashMap<Integer, Node> map = new HashMap<>();
+    
     public Node cloneGraph(Node node) {
-        if (node == null) { return node; }
-        if (node.neighbors.isEmpty()) { return copyOf(node); }
-
-        Node[] visited = new Node[101];
-        Queue<Node> q = new ArrayDeque<Node>();
-        Node startNode = node;
-        q.offer(node);
-        while (!q.isEmpty()) {
-            Node vertex = q.poll();
-            if (visited[vertex.val] == null) {
-                for (Node adj : vertex.neighbors) {
-                    q.offer(adj);
-                }
-                visited[vertex.val] = vertex;
-            }
-        }
-        for (Node n : visited) {
-            if (n != null) {
-                visited[n.val] = copyOf(n);
-            }            
-        }
-        for (int i=1; i<101; i++) {
-            Node n = visited[i];
-            if (n == null) {
-                continue;
-            }            
-            if (i == 1) {
-                startNode = n;
-            }
-            List<Node> newAdjs = new ArrayList<Node>();
-            for (Node adj : n.neighbors) {
-                newAdjs.add(visited[adj.val]);
-            }
-            n.neighbors = newAdjs;
-        }
-        return startNode;
+        return clone(node);
     }
-
-    public Node copyOf(Node origin) {
-        Node result = new Node(origin.val);
-        result.neighbors.addAll(origin.neighbors);
-        return result;
+    
+    public Node clone(Node node) {
+        if (node == null) return null;
+        
+        if (map.containsKey(node.val)) 
+            return map.get(node.val);
+        
+        Node newNode = new Node(node.val, new ArrayList<Node>());
+        map.put(newNode.val, newNode);
+        for (Node neighbor : node.neighbors) 
+            newNode.neighbors.add(clone(neighbor));
+        return newNode;
     }
 }
