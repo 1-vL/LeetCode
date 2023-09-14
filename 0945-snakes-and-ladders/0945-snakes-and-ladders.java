6b154862244df03b57346ddf7d57271afcc18324
Jun <92934376+1-vL@users.noreply.github.com>
2023-09-14 18:45:02 +0900

    Time: 5 ms (83.85%), Space: 43.1 MB (89.32%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.java
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -1,52 +1,40 @@
-class Solution {
-    public int snakesAndLadders(int[][] board) {
-        int n = board.length;
-        int size=n*n;
-        int[] oneDimension = new int[size+1];
-        boolean[] visited = new boolean[size+1];
-        int cnt = 1;
-        // 1차원 배열로 변환
-        for (int i=n-1; i>=0; i--) {
-            if ((n-i)%2==1) {
-                // 홀수행(좌->우)
-                for (int j=0;j<n;j++) {
-                    oneDimension[cnt++] = board[i][j];
-                }
-            } else {     
-                // 짝수행(우->좌)
-                for (int j=n-1;j>=0;j--) {
-                    oneDimension[cnt++] = board[i][j];
-                }
+import java.util.*;
+class Solution 
+{
+    public int snakesAndLadders(int[][] board) 
+    {
+        int rows = board.length, cols = board[0].length, target = rows * cols, r, c, p;
+        boolean[] visited = new boolean[rows * cols + 1]; // cells on board start from 1
+        // queue contains <priority, square> sorted ascending by priority
+        // prio = #steps * 1000 + (500 - square);
+        // number of steps is critical and adds 1000; 500 is selected as it is higher than the max cell (20*20=400)
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        q.offer(new int[] {0, 1}); // 0 steps to position 1
+        visited[1] = true;
+
+        while (!q.isEmpty()) 
+        {
+            int[] step_pos = q.poll();
+            int s = step_pos[0] / 1000 + 1;
+            for (int i = 1; i <= 6; i++) 
+            {
+                p = step_pos[1] + i;
+                if (visited[p]) continue;
+                visited[p] = true;
+
+                r = rows - (p - 1) / cols - 1;
+                c = (p - 1) % cols;
+                if ((rows - r - 1) % 2 == 1) 
+                    c = cols - c - 1; // change direction for odd lines
+                int ladder = board[r][c];
+                
+                if (ladder > 0 && ladder <= target)
+                    p = ladder; // no update for steps. allow to come here again with a dice
+                if (p == target) 
+                    return s;
+                q.offer(new int[] {s * 1000 + 500 - p, p});
             }
         }
-
-        Queue<int[]> q = new ArrayDeque<int[]>();
-        q.offer(new int[] {1,0});
-
-        while (!q.isEmpty()) {
-            int[] before = q.poll();
-            int cur = before[0];
-            int move_cnt = before[1];            
-            if (visited[cur]) {
-                continue;
-            }
-            if (cur == size) {
-                return move_cnt;
-            }
-            visited[cur] = true;
-
-            int dice_move = Math.min(size, cur+6);
-            
-            for (int i=cur+1; i<=dice_move; i++) {
-                if (oneDimension[i] != -1) {                
-                    // 뱀 또는 사다리
-                    q.offer(new int[] {oneDimension[i], move_cnt+1});
-                } else {
-                    q.offer(new int[] {i, move_cnt+1});
-                }
-            }
-        }
-
         return -1;
     }
 }
